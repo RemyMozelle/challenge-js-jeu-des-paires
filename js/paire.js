6e35d99010424567afccb878348bac5e2572b717
remy <timoz974@hotmail.fr>
2017-07-21 19:35:54 +0300

    modif

--- js/paire.js
+++ js/paire.js
@@ -1,8 +1,11 @@
-class Jeu {
-
-}
-
 let jeu_1 = [
+    "img/ane.jpg",
+    "img/chat.jpg",
+    "img/chien.jpg",
+    "img/lama.jpg",
+    "img/lapins.jpg",
+    "img/lionne.jpg",
+    "img/ours.jpg",
     "img/ane.jpg",
     "img/chat.jpg",
     "img/chien.jpg",
@@ -12,10 +15,17 @@ let jeu_1 = [
     "img/ours.jpg"
 ]
 
-
-
 let cartes = document.querySelectorAll('img');
+let compteur = 2;
+for (let i = 0; i < cartes.length; i++) {
+    cartes[i].setAttribute('src', 'img/carte.png');
+    cartes[i].addEventListener('click', () => {
+        cartes[i].setAttribute('src', jeu_1[i]);
+        
+        window.setTimeout(function() {
+            if(cartes[i] === jeu_1[i]){ console.log('g')}
+        }, 1000);
 
-for (let i = 0; i < cartes.length; i++){
-    cartes[i].setAttribute('src', jeu_1[i]);
+        
+    })
 }
